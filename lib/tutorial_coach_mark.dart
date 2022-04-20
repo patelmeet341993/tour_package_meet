4ba9fbc660585c71f69daf4c780bed46bbc09e18
patelmeet341993 <patelmeet341993@gmail.com>
2022-04-20 11:36:40 +0530

    bottom fix

--- lib/tutorial_coach_mark.dart
+++ lib/tutorial_coach_mark.dart
@@ -55,27 +55,24 @@ class TutorialCoachMark {
   OverlayEntry _buildOverlay() {
     return OverlayEntry(
       builder: (context) {
-        return Padding(
-          padding: EdgeInsets.only(top: 50),
-          child: TutorialCoachMarkWidget(
-            key: _widgetKey,
-            targets: targets,
-            clickTarget: onClickTarget,
-            clickOverlay: onClickOverlay,
-            paddingFocus: paddingFocus,
-            onClickSkip: skip,
-            alignSkip: alignSkip,
-            skipWidget: skipWidget,
-            textSkip: textSkip,
-            textStyleSkip: textStyleSkip,
-            hideSkip: hideSkip,
-            colorShadow: colorShadow,
-            opacityShadow: opacityShadow,
-            focusAnimationDuration: focusAnimationDuration,
-            pulseAnimationDuration: pulseAnimationDuration,
-            pulseEnable: pulseEnable,
-            finish: finish,
-          ),
+        return TutorialCoachMarkWidget(
+          key: _widgetKey,
+          targets: targets,
+          clickTarget: onClickTarget,
+          clickOverlay: onClickOverlay,
+          paddingFocus: paddingFocus,
+          onClickSkip: skip,
+          alignSkip: alignSkip,
+          skipWidget: skipWidget,
+          textSkip: textSkip,
+          textStyleSkip: textStyleSkip,
+          hideSkip: hideSkip,
+          colorShadow: colorShadow,
+          opacityShadow: opacityShadow,
+          focusAnimationDuration: focusAnimationDuration,
+          pulseAnimationDuration: pulseAnimationDuration,
+          pulseEnable: pulseEnable,
+          finish: finish,
         );
       },
     );
